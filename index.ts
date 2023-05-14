b3c01580cb466614f39b475ad9b3606c7c137a94
Ryan Braun <ryan.m.braun@hotmail.com>
2023-05-14 04:06:11 -0400

    extract extendsFrom to curried call

--- index.ts
+++ index.ts
@@ -12,7 +12,9 @@ type RawCSSProperties = {
 
 type Props = Record<string, unknown>
 
-type StyledFunction<P = Props> = (props: P) => (CSSProperties & RawCSSProperties & Record<string, unknown>) | undefined
+type CssFunction<P extends Props = any> = (
+  props: P
+) => (CSSProperties & RawCSSProperties & Record<string, unknown>) | undefined
 
 type TagNames = keyof JSX.IntrinsicElements
 
@@ -24,120 +26,115 @@ type StyledComponent<Tag extends CustomTag | TagNames, P extends { css?: Props }
   P & (Tag extends TagNames ? JSX.IntrinsicElements[Tag] : BasicElementAttributes)
 > & {
   className: string | undefined
-  styleFunction: StyledFunction<any>
+  styleFunction: CssFunction
 }
 
-type Styled<Tag extends string = CustomTag> = {
-  (): StyledComponent<Tag, {}>
-
-  (extendsFrom: string | Array<string | null | undefined>): StyledComponent<Tag, { css?: undefined }>
-
-  <ExtendsCssProps extends { css?: Props }>(extendsFrom: StyledComponent<any, ExtendsCssProps>): StyledComponent<
+type Styled<Tag extends string = string, ExtendsCssProps extends { css?: Props } = {}> = {
+  (classNames?: string | null | undefined | Array<string | null | undefined>): StyledComponent<
     Tag,
     ExtendsCssProps['css'] extends undefined ? {} : ExtendsCssProps
   >
 
   <CssProps extends Props>(
-    extendsFrom: string | Array<string | null | undefined>,
-    styleFunction: StyledFunction<CssProps>
-  ): StyledComponent<Tag, { css: CssProps }>
-
-  <CssProps extends Props, ExtendsCssProps extends { css?: Props }>(
-    extendsFrom: StyledComponent<any, ExtendsCssProps>,
-    styleFunction: StyledFunction<CssProps>
-  ): StyledComponent<
-    Tag,
-    ExtendsCssProps['css'] extends undefined ? { css: CssProps } : { css: CssProps & ExtendsCssProps['css'] }
-  >
-
-  <CssProps extends Props, ExtendsCssProps extends { css?: Props }>(
-    extendsFrom?: string | StyledComponent<any, ExtendsCssProps> | Array<string | null | undefined> | null | undefined,
-    styleFunction?: StyledFunction<CssProps> | null | undefined
+    classNames: string | null | undefined | Array<string | null | undefined>,
+    styleFunction?: CssFunction<CssProps>
   ): StyledComponent<
     Tag,
     ExtendsCssProps['css'] extends undefined ? { css: CssProps } : { css: CssProps & ExtendsCssProps['css'] }
   >
 }
 
-export default new Proxy(
-  function (
-    tagName: string,
-    extendsFrom?: string | StyledComponent<any, any> | Array<string | null | undefined> | null | undefined,
-    styleFunction?: StyledFunction<any> | null | undefined
-  ) {
-    if (typeof tagName !== 'string') {
-      throw new Error('tagName must be a valid JSX element tag name')
-    }
+const getMergedStyleFunction =
+  (...funcs: Array<CssFunction | undefined | null>) =>
+  (css?: Props): Record<string, unknown> =>
+    Object.assign({}, ...funcs.map((func) => func?.(css) ?? {}))
 
-    if (
-      extendsFrom != null &&
-      typeof extendsFrom !== 'string' &&
-      !(Array.isArray(extendsFrom) && extendsFrom.every((value) => value == null || typeof value === 'string')) &&
-      !('styleFunction' in extendsFrom)
-    ) {
-      throw new Error('extendsFrom must be a String, StyledComponent, or Array<String>')
-    }
-
-    if (styleFunction != null && typeof styleFunction !== 'function') {
-      throw new Error('styleFunction must be a function that returns CSSProperties')
-    }
-
-    const parentClassName =
-      extendsFrom == null
-        ? undefined
-        : typeof extendsFrom === 'string'
-        ? extendsFrom.trim()
-        : Array.isArray(extendsFrom)
-        ? Array.from(
-            new Set(
-              extendsFrom.flatMap((value) => value?.split(' ')?.map((value) => value.trim())).filter((value) => !!value)
-            )
-          ).join(' ')
-        : extendsFrom.className
-
-    const parentStyleFunction =
-      extendsFrom == null ||
-      typeof extendsFrom === 'string' ||
-      Array.isArray(extendsFrom) ||
-      extendsFrom.styleFunction == null
-        ? null
-        : extendsFrom.styleFunction
-
-    const mergedStyleFunction = (css: Props = {}) =>
-      Object.assign({}, parentStyleFunction?.(css) ?? {}, styleFunction?.(css) ?? {})
-
-    const component = React.memo(
-      React.forwardRef(function Styled(props: { css?: Props; className?: string; style: CSSProperties }, ref) {
-        const { css, className, style, ...rest } = props
-
-        return React.createElement(tagName, {
-          ...rest,
-          ref,
-          className: React.useMemo(() => `${className || ''} ${parentClassName || ''}`.trim(), [className]),
-          style: React.useMemo(
-            () =>
-              Object.fromEntries(
-                Object.entries(mergedStyleFunction(css)).map(([key, value]) => [
-                  key.startsWith('_') ? key.slice(1) : `--${key}`,
-                  value,
-                ])
-              ),
-            [css]
-          ),
-        })
-      })
-    ) as unknown as StyledComponent<string, any>
-
-    component.className = parentClassName
-    component.styleFunction = mergedStyleFunction
-
-    return component
-  },
-  {
-    get(target, key) {
-      return (...args: any[]) => target(key.toString(), ...args)
-    },
+function createdStyledComponent<T extends CustomTag | TagNames>(
+  tagName: string,
+  extendsFrom?: StyledComponent<any, any> | null | undefined,
+  classNames?: string | Array<string | null | undefined> | null | undefined,
+  styleFunction?: CssFunction | null | undefined
+) {
+  if (typeof tagName !== 'string') {
+    throw new Error('tag name must be a string')
   }
-) as unknown as {
+
+  if (extendsFrom != null && !('styleFunction' in extendsFrom)) {
+    throw new Error('can only extend from another StyledComponent')
+  }
+
+  if (
+    classNames != null &&
+    typeof classNames !== 'string' &&
+    !(Array.isArray(classNames) && classNames.every((value) => value == null || typeof value === 'string'))
+  ) {
+    throw new Error('classNames must be a string or an array of strings')
+  }
+
+  if (styleFunction != null && typeof styleFunction !== 'function') {
+    throw new Error('styleFunction must be a function that returns CSSProperties')
+  }
+
+  const mergedClassNames = Array.from(
+    new Set(
+      [extendsFrom?.className, classNames]
+        .flat()
+        .filter((value): value is string => typeof value === 'string')
+        .flatMap((value) => value?.split(' ')?.map((value) => value.trim()))
+        .filter((value) => !!value)
+    )
+  ).join(' ')
+
+  const mergedStyleFunction = getMergedStyleFunction(extendsFrom?.styleFunction, styleFunction)
+
+  const component = React.memo(
+    React.forwardRef(function Styled(props: { css?: Props; className?: string; style: CSSProperties }, ref) {
+      const { css, className, style, ...rest } = props
+
+      return React.createElement(tagName, {
+        ...rest,
+        ref,
+        className: React.useMemo(() => `${className || ''} ${mergedClassNames || ''}`.trim(), [className]),
+        style: React.useMemo(
+          () =>
+            Object.fromEntries(
+              Object.entries(mergedStyleFunction(css)).map(([key, value]) => [
+                key.startsWith('_') ? key.slice(1) : `--${key}`,
+                value,
+              ])
+            ),
+          [css]
+        ),
+      })
+    })
+  ) as unknown as StyledComponent<T, any>
+
+  component.className = mergedClassNames
+  component.styleFunction = mergedStyleFunction
+
+  return component
+}
+
+const styledProxy = <ExtendsCssProps extends { css?: Props } = {}>(
+  extendsFrom?: StyledComponent<any, ExtendsCssProps>
+) =>
+  new Proxy(
+    {},
+    {
+      get(_, key: string) {
+        return (...args: any[]) => createdStyledComponent(key, extendsFrom, ...args)
+      },
+    }
+  ) as {
+    [K in keyof JSX.IntrinsicElements]: Styled<K, ExtendsCssProps>
+  } & { [K: string]: Styled<string, ExtendsCssProps> }
+
+const styled = new Proxy(styledProxy, {
+  get(target, key: string) {
+    return (...args: any[]) => target()?.[key]?.(...args)
+  },
+}) as typeof styledProxy & {
   [K in keyof JSX.IntrinsicElements]: Styled<K>
-} & { [K: string]: Styled }
+} & { [K: string]: Styled<string> }
+
+export default styled
